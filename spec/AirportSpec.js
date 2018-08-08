7ff1bbacfea82fe59d50f29b8d637740d6cfb7c1
edthomas93 <et1270@my.bristol.ac.uk>
2018-08-08 18:19:30 +0100

    All user stories satisfied

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -3,11 +3,13 @@
 describe("Airport", function() {
 
   var airport;
+  var airport10;
   var plane;
 
   beforeEach(function() {
     plane = {};
     airport = new Airport();
+    airport10 = new Airport(10);
   });
 
 
@@ -17,9 +19,25 @@ describe("Airport", function() {
       airport.land(plane);
       expect(airport.planes).toContain(plane);
     });
-    it('lands a plane if weather is not stormy', function() {
+    it('does not land a plane if weather is stormy', function() {
       spyOn(Math, 'random').and.returnValue(0.95);
       expect(function() { airport.land(plane) }).toThrowError("Weather too stormy to land");
+    });
+    it('does not land a plane if full using default capacity', function() {
+      spyOn(Math, 'random').and.returnValue(0.6);
+      for (var i = 1; i < 6; i++) {
+        airport.land(plane);
+      };
+      expect(airport.planes.length).toEqual(5);
+      expect(function() { airport.land(plane) }).toThrowError("Airport too full to land");
+    });
+    it('does not land a plane if full using capacity 10', function() {
+      spyOn(Math, 'random').and.returnValue(0.6);
+      for (var i = 1; i < 11; i++) {
+        airport10.land(plane);
+      };
+      expect(airport10.planes.length).toEqual(10);
+      expect(function() { airport10.land(plane) }).toThrowError("Airport too full to land");
     });
   });
 
@@ -44,6 +62,15 @@ describe("Airport", function() {
       spyOn(airport.weather, 'stormy');
       airport.stormy();
       expect(airport.weather.stormy).toHaveBeenCalled();
+    });
+  });
+
+  describe('#capacity', function() {
+    it('defaults to 5', function() {
+      expect(airport.capacity).toEqual(5);
+    });
+    it('capacity is set to argument passed upon creation of airport', function() {
+      expect(airport10.capacity).toEqual(10);
     });
   });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,11 +1,15 @@
-function Airport() {
+function Airport(capacity = 5) {
   this.planes = [];
   this.weather = new Weather();
+  this.capacity = capacity;
 };
 
 Airport.prototype.land = function (plane) {
   if (this.stormy()) {
     throw new Error('Weather too stormy to land');
+  }
+  else if (this.planes.length >= this.capacity) {
+    throw new Error('Airport too full to land');
   }
   else {
     this.planes.push(plane);
